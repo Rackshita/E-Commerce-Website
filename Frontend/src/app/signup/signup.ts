2feb21ffa26b2d33b23865477cf005aced95bff0
Rackshita <rackshitha17@gmail.com>
2026-01-19 11:20:55 +0530

    signup

--- Frontend/src/app/signup/signup.ts
+++ Frontend/src/app/signup/signup.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Auth } from '../services/auth/auth';
+import { MATERIAL_IMPORTS } from '../material-imports';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+   imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ...MATERIAL_IMPORTS
+
+  ],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
 export class Signup {
+signupForm !: FormGroup;
+hidePassword = true;
+
+constructor( private fb: FormBuilder,
+private snackBar: MatSnackBar,
+private authService: Auth,
+private router: Router){
+
+}
+
+ngOnInit(): void {
+this.signupForm = this.fb.group({
+name: [null, [Validators.required]],
+email: [null, [Validators.required, Validators.email]],
+password: [null, [Validators.required]],
+confirmPassword: [null, [Validators.required]],
+})
+}
+togglePasswordVisibility(){
+  this.hidePassword = !this.hidePassword;
+}
+
+onSubmit(): void {
+const password = this.signupForm.get('password') ?. value;
+const confirmPassword = this.signupForm.get('confirmPassword') ?. value;
+
+if(password !== confirmPassword){
+this.snackBar.open('Passwords do not match.', 'Close', { duration: 5000, panelClass: 'error-snackbar' });
+return;
+
+}
+
+this.authService.register(this.signupForm.value).subscribe(
+(response) =>{
+this.snackBar.open('Sign up successful!', 'Close', { duration: 5000});
+this.router.navigateByUrl("/login");
+},
+(error)=>{
+this.snackBar.open('Sign up failed. Please try again.','Close',{duration: 5000, panelClass:'error-snackbar'});
+}
+)
+}
+
 
 }
